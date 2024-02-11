0595fecf7a75d5ba03a27ef7f57a2da0de504c42
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-02-12 05:58:28 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -63,17 +63,17 @@ else:
 #                       'filter5', 'filter6', 'filter7', 'filter8',
 #                       'filter9', 'filter10', 'filter11', 'filter12',
 #                       'filter13', 'filter14']
-data1 = data[data['GRP'] == 'Set1']['pred_day', 'coin', 'RE_RANK', 'RE_RANK_UP', 
+data1 = data[data['GRP'] == 'Set1'][['pred_day', 'coin', 'RE_RANK', 'RE_RANK_UP', 
                     'NO_UP_HIGH1', 'NO_UP_CL16', 'NO_UP_HIGH16', 'NO_UP_LOW16', 'NO_UP_HCL16',
                       'NO_DOWN', 'NO_DOWN_CL16',  'NO_DOWN_LOW16',
                       'filter1', 'filter2', 'filter3', 'filter4',
                       'filter5', 'filter6', 'filter7', 'filter8',
                       'filter9', 'filter10', 'filter11', 'filter12',
-                      'filter13', 'filter14']
+                      'filter13', 'filter14']]
 st.write(data1)
 
 # selected_columns3 = ['pred_day', 'coin', 'SEQ', 'date', 'close_up', 'high_up', 'low_up' ]
-data3 = data[data['GRP'] == 'Set3']['pred_day', 'coin', 'SEQ', 'date', 'close_up', 'high_up', 'low_up' ]
+data3 = data[data['GRP'] == 'Set3'][['pred_day', 'coin', 'SEQ', 'date', 'close_up', 'high_up', 'low_up' ]]
 
 
 
