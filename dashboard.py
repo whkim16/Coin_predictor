810d3d0fc90408b6722f77aec193388ac8a9bbe7
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-02-12 11:25:51 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -86,11 +86,13 @@ select_date = st.sidebar.selectbox(
     'Select Date',
     data1['pred_day'].sort_values(ascending=True).unique()
 )
+values = st.slider('Select a range of values', 0, len(data1['coin'].unique()), (25, 75))
+
 data1 = data1.rename(columns={'RE_RANK': '추천순서1'})
 data1 = data1.rename(columns={'RE_RANK_UP': '추천순서2'})
 data1 = data1.rename(columns={'pred_day': '예측일'})
 
-st.write(data1[data1['예측일'] == select_date] )
+st.write(data1[(data1['예측일'] == select_date) & (data1['추천순서1'].isin(values))  ] )
 
 # 여러개 선택할 수 있을 때는 multiselect를 이용하실 수 있습니다 
 # return : list
