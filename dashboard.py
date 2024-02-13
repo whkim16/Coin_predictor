0fbcccd8445ac5addeeb403601a7ceb52f0f7407
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-02-13 21:54:54 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -293,7 +293,7 @@ with col1 :
     ax.set_xticklabels(ax.get_xticks(), rotation=45, ha='right')
     ax.set_xlabel("date")
     ax.set_ylabel("close")
-    ax.set_title(f'close 6 day predict date:  {select_date} ')  # (f' 예측날짜:  {select_date} 9시 기준')
+    ax.set_title(f' 6 day predict date:  {select_date} ')  # (f' 예측날짜:  {select_date} 9시 기준')
     # plt.xticks([])  # Disable x-axis ticks
     # plt.yticks([])  # Disable y-axis ticks
     ax.grid(True)
