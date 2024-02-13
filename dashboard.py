08aa1f0a3537715ba4566b3df64ebeaac0992831
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-02-13 19:55:48 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -31,9 +31,9 @@ st.title("코인예측 시뮬레이션 데시보드")
 st.markdown(f'### 1. 코인 추천랭킹, 예측날짜: {formatted_date} 9시 기준')
 # 사이드바에 select box를 활용하여 종을 선택한 다음 그에 해당하는 행만 추출하여 데이터프레임을 만들고자합니다.
 st.sidebar.title("Coin Chart")
-st.sidebar.markdown('코인심볼 Link : [All Coin Symbols](https://upbit.com/exchange?code=CRIX.UPBIT.KRW-BTC)')
-st.sidebar.markdown('코스피심볼 Link : [All Kospi200 Symbols](https://finance.naver.com/sise/sise_index.nhn?code=KPI200)')
-st.sidebar.markdown('나스닥심볼 Link : [All Nasdaq200 Symbols](https://kr.investing.com/indices/nq-100-components)')
+st.sidebar.markdown('비트/알트코인 Link : [All Coin Symbols](https://upbit.com/exchange?code=CRIX.UPBIT.KRW-BTC)')
+st.sidebar.markdown('코스피200 Link : [All Kospi200 Symbols](https://finance.naver.com/sise/sise_index.nhn?code=KPI200)')
+st.sidebar.markdown('나스닥200 Link : [All Nasdaq200 Symbols](https://kr.investing.com/indices/nq-100-components)')
 
 
 
