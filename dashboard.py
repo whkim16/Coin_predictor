7eb5fc980b7286afcf0feb6292e0ac3c05a48cf2
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-02-17 19:52:22 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -179,8 +179,8 @@ data4_1CLx = ['2024-' + text[:5] for text in data4_1CLx.date ]
 
 # ax.plot(data4_1CLx[:(len(data4_1CLx) - 6)], (data4_1CLy.value_close[:(len(data4_1CLx) - 6)]), linestyle='-', marker='*', color='black', linewidth=2)
 # ax.plot(data4_1CLx[:(len(data4_1CLy0.value_close))], (data4_1CLy0.value_close), linestyle='-', marker='*', color='black', linewidth=2)
-data4_1CLy0 = data4_1[data4_1['variable'] == 'Pred0'][['value_close']]
 
+data4_1CLy0 = data4_1[data4_1['variable'] == 'Pred0'][['value_close']]
 
 data4_1CLy = data4_1[data4_1['variable'] == 'Pred1'][['value_close']]
 
@@ -312,7 +312,7 @@ with col1 :
     ax.tick_params(axis='both', which='both', length=1, width=0.5)
 
     ax.plot(data4_1CLx[:(len(data4_1CLx) - 6)], (data4_1CLy.value_close[:(len(data4_1CLx) - 6)]), linestyle='-', marker='*', color='black', linewidth=2)
-    ax.plot(data4_1CLx[:(len(data4_1CLy0.value_close)+1)], (data4_1CLy0.value_close), linestyle='-', marker='*', color='black', linewidth=2)
+    ax.plot(data4_1CLx[:(len(data4_1CLy0.value_close))], (data4_1CLy0.value_close), linestyle='-', marker='*', color='black', linewidth=2)
     # plt.show()
     st.pyplot(fig)
 with col2 :
