e884d83a7c73806b9b0725d4e4c96038fa60a97b
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-02-19 21:21:26 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -509,7 +509,8 @@ with col4 :
 
 st.markdown(f'#### 참고. 투자코인 , 예측날짜:  {formatted_date} 9시 기준')
 
-select_multi_coin2 = ['ETH','LSK','EOS','QTUM','BTT','LINK','STEEM','POWR','ETC','MBL','NEO','PLA','IMX','SC','FLOW']
+select_multi_coin2 = ['ETH','LSK','EOS','QTUM','BTT','LINK','STEEM','POWR','ETC','MBL','NEO','PLA','IMX','SC','FLOW',
+                     '코웨이','현대미포조선']
 data3_2 = data3[ (data3['coin'].isin(select_multi_coin2))  & (data3['예측일']==select_date)  &  (data3['SEQ'] >= min(seqs)) & (data3['SEQ'] <= max(seqs))]
 data3_2 = data3_2.sort_values(by='high_up', ascending=True)
 data3_2.index = [''] * len(data3_2)
