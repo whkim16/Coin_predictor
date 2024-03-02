c378c733fc1452f9893952aa055d643531372b21
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-02 20:08:58 +0900

    Update monitoring.py

--- monitoring.py
+++ monitoring.py
@@ -338,11 +338,7 @@ data_coin_a_pv2 = data_coin_a_pv2.sort_values(by=real_uprate2, ascending = False
 
 
 if select_coin2 == 'all':
-    col1,col2 = st.columns([1,1])
-    with col1 :
       st.write(data_coin_a_pv2)
-    with col2 :
-      st.write(data_coin_ccf)
 else:
     st.write(data_coin_a_pv2[ (data_coin_a_pv2['coin']  == select_coin2) ])
 
@@ -359,10 +355,18 @@ select_coin2_2 = st.selectbox(
 
 # 
 if  (select_coin2_2 == 'all'):  # (select_coin2_1 == 'all') |
+    col1,col2 = st.columns([1,1])
+    with col1 :
     st.write(data_coin_cr) 
+    with col2 :
+    st.write(data_coin_ccf)
 else:
+    col1,col2 = st.columns([1,1])
+    with col1 : 
     st.write(data_coin_cr[ (data_coin_cr['기준코인']  == select_coin2_1) & (data_coin_cr['대상코인']  == select_coin2_2) ])
-
+    with col2 :
+    st.write(data_coin_ccf[ (data_coin_ccf['기준코인']  == select_coin2_1) & (data_coin_ccf['대상코인']  == select_coin2_2) ])
+  
 select_coin2_3 = st.selectbox(
     '▷ 3. 크로스 비교 지표 선택 (고점/종가/저점) ',
     ['high','close','low']
