1065d0eaa3714a89be752007a0409db2e4ce0f72
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-02 16:30:45 +0900

    Update monitoring.py

--- monitoring.py
+++ monitoring.py
@@ -397,6 +397,46 @@ else:
 
 
 
+
+
+
+
+# 샘플 데이터 생성
+x = np.arange(0, 10, 0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+# 첫 번째 축에 그래프 그리기
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('X-axis')
+ax1.set_ylabel('Sin', color=color)
+ax1.plot(x, y1, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+# 두 번째 축 생성
+ax2 = ax1.twinx()
+
+color = 'tab:blue'
+ax2.set_ylabel('Cos', color=color)
+ax2.plot(x, y2, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+plt.title('Sin and Cos Functions')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
 st.markdown(f'##### ------------------------------------------------------------------------------------------------------  ')
 st.markdown(f'##### ------------------------------------------------------------------------------------------------------  ')
 
