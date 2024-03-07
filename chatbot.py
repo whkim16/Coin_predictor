a78477ea0eb40247bb669bc0c117d033c82140c0
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-07 16:14:24 +0900

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+
+
+import streamlit as st
+from streamlit_chat import message
+import pandas as pd
+from sentence_transformers import SentenceTransformer #sentenceBERT 모델 사용
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+
+
+
+
+
