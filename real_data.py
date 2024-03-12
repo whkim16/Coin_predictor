9f5a82b93b54827256392765a799f5f2d85aa9b1
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-12 21:53:17 +0900

    Update real_data.py

--- real_data.py
+++ real_data.py
@@ -53,7 +53,7 @@ coin4 = 'KRW-ELF'
 coin5 = select_coin
 
 # st.write(1)
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+st.write("My secrets:", st.secrets["my_secrets"]["secret_code"])
 
 
 
