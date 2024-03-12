5956bc1aacc05ab2aeed27531159d8d6adb363fe
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-12 22:07:43 +0900

    Update real_data.py

--- real_data.py
+++ real_data.py
@@ -37,7 +37,7 @@ st.sidebar.markdown('나스닥200 Link : [All Nasdaq200 Symbols](https://kr.inve
 
 text_input = st.text_input("암호를 입력하세요", "기본값")
 
-st.write(st.secrets["my_secrets"]["secret_code"]['0'])
+st.write(st.secrets["my_secrets"]["secret_code"][0])
 st.write("My secrets:", st.secrets["my_secrets"]["secret_code"])
 
 
