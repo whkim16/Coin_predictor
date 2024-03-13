3e7d8def5cda258e26c9f4e89ee43a97881ca624
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-13 16:42:15 +0900

    Update real_data.py

--- real_data.py
+++ real_data.py
@@ -15,10 +15,10 @@ import pyupbit
 
 hide_streamlit_style = """
             <style>
-            MainMenu {visibility: hidden;}
+            #MainMenu {visibility: hidden;}
             #footer {visibility: hidden;}
             GithubIcon {visibility: hidden;}
-            #header {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
