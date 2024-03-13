52b1e3fa1eaba7cb160210ea44ccb9174102d764
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-13 14:55:18 +0900

    Update real_data.py

--- real_data.py
+++ real_data.py
@@ -11,14 +11,23 @@ import numpy as np
 import time
 import pyupbit
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            #footer {visibility: hidden;}
-            #GithubIcon {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+# hide_streamlit_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             #footer {visibility: hidden;}
+#             #GithubIcon {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 
 
