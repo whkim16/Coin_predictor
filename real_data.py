73bcaa48ea1ed06430365809b6dd97e33dc0e599
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-13 15:02:40 +0900

    Update real_data.py

--- real_data.py
+++ real_data.py
@@ -36,18 +36,13 @@ import pyupbit
 #             """
 # st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.markdown("""
+        <style>
+        .css-15zrgzn {display: none}
+        .css-eczf16 {display: none}
+        .css-jn99sy {display: none}
+        </style>
+        """, unsafe_allow_html=True)
 
 
 
