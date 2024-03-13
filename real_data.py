672924457c9faed2d1116c08b0fafefcccb24bcb
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-13 14:56:35 +0900

    Update real_data.py

--- real_data.py
+++ real_data.py
@@ -20,6 +20,13 @@ hide_github_icon = """
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
+
+hide_github_icon = """
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+ """
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 # hide_streamlit_style = """
 #             <style>
 #             #MainMenu {visibility: hidden;}
