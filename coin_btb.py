ae3bb0ab9127881de8ba2097976b8ffa3979b244
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-03-18 23:05:47 +0900

    Update coin_btb.py

--- coin_btb.py
+++ coin_btb.py
@@ -67,7 +67,7 @@ formatted_date = today.strftime("%Y-%m-%d")
 
 # Streamlit Markdown에 날짜 추가
 st.title("코인예측 시뮬레이션 데시보드")
-st.markdown(f'### 1. 코인 추천랭킹, 예측날짜: {formatted_date} 9시 기준')
+st.markdown(f'### 1. 코인 추천랭킹, 예측날짜: {formatted_date} 00시 기준')
 # 사이드바에 select box를 활용하여 종을 선택한 다음 그에 해당하는 행만 추출하여 데이터프레임을 만들고자합니다.
 st.sidebar.title("Coin Chart")
 st.sidebar.markdown('비트/알트코인 Link : [All Coin Symbols](https://upbit.com/exchange?code=CRIX.UPBIT.KRW-BTC)')
@@ -274,7 +274,7 @@ st.write(data1[(data1['예측일'] == select_date) &  (data1['추천순서1'] >=
             (data1['filter3'] >= min(values03) ) & (data1['filter3'] <= max(values03) ) &
             (data1['filter4'] >= min(values02) ) & (data1['filter4'] <= max(values02) ) ] )
 
-st.markdown(f'### 2. 코인 주요변수(다중), 예측날짜: {formatted_date} 9시 기준')
+st.markdown(f'### 2. 코인 주요변수(다중), 예측날짜: {formatted_date} 00시 기준')
 # 여러개 선택할 수 있을 때는 multiselect를 이용하실 수 있습니다 
 # return : list
 select_multi_coin = st.multiselect(   # st.sidebar.multiselect(
@@ -311,7 +311,7 @@ data3 = data3.rename(columns={'low_up': '저점상승확률'})
 # 
 # 원래 dataframe으로 부터 꽃의 종류가 선택한 종류들만 필터링 되어서 나오게 일시적인 dataframe을 생성합니다
 data3_1 = data3[ (data3['coin'].isin(select_multi_coin))  & (data3['예측일']==select_date)  &  (data3['SEQ'] >= min(seqs)) & (data3['SEQ'] <= max(seqs))]
-st.markdown(f'### 3. 매수매도결정 , 예측날짜:  {formatted_date} 9시 기준')
+st.markdown(f'### 3. 매수매도결정 , 예측날짜:  {formatted_date} 00시 기준')
 
 data3_1.index = [''] * len(data3_1)
 
