30454dfa9e159e1481437a689b5b8beb39f77227
whkim16 <53405760+whkim16@users.noreply.github.com>
2024-04-03 15:16:01 +0900

    Update coin_btb.py

--- coin_btb.py
+++ coin_btb.py
@@ -337,7 +337,6 @@ data4 = data[data['GRP'] == 'Set4' ][['GRP', 'pred_day', 'coin', 'SEQ', 'date', 
 # select_species 변수에 사용자가 선택한 값이 지정됩니다
 select_coin = st.selectbox(
     '👈 Select Coin Symbols For #4',
-    # data4['coin'].sort_values(ascending=True).unique()
     ['BTC'] + list(data1[(data1['예측일'] == select_date) ].sort_values(by='추천순서1', ascending=True).coin.unique())
 )
 
